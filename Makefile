1c4ca9fc2d0cf0aae44595b7bbe9c51c363ac459
akhilmhdh <akhilmhdh@gmail.com>
2023-01-20 14:58:30 +0530

    feat: implemented configuration loading and flag to load from any directory

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 apc:
 	go run cmd/cli/main.go 
+
+test:
+	go run cmd/cli/main.go -a graphql --url ./test --config ./cmd/cli 
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,12 +3,14 @@ package cli
 import (
 	_ "embed"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/dop251/goja"
 	"github.com/dop251/goja_nodejs/console"
 	"github.com/dop251/goja_nodejs/require"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // Plugin API Design Spec Sheet
@@ -20,14 +22,35 @@ import (
 //go:embed babel.min.js
 var babelBundle string
 
+type ApiCatalogConfig struct {
+	Title string
+}
+
 func Run() {
+	// cli flags
 	var apiType string
 	var apiURL string
+	var configFilePath string
 
 	rootCmd := &cobra.Command{
-		Use:   "apc",
+		Use:   "apic",
 		Short: "One shot cli for your api schema security,performance and quality check",
 		Run: func(cmd *cobra.Command, args []string) {
+			// find config file and load up the config
+			var config ApiCatalogConfig
+
+			viper.AddConfigPath(configFilePath)
+			viper.SetConfigName("apic")
+
+			if err := viper.ReadInConfig(); err != nil {
+				log.Fatal("File not found", err)
+			}
+
+			if err := viper.Unmarshal(&config); err != nil {
+				log.Fatal("Error in config file", err)
+			}
+
+			fmt.Println(config.Title)
 			fmt.Println(apiType)
 		},
 	}
@@ -37,6 +60,8 @@ func Run() {
 
 	rootCmd.PersistentFlags().StringVar(&apiURL, "url", "", "URL or local file containing spec sheet")
 	rootCmd.MarkPersistentFlagRequired("url")
+
+	rootCmd.PersistentFlags().StringVar(&configFilePath, "config", ".", "Path to apic configuration file")
 
 	reqistry := new(require.Registry)
 
